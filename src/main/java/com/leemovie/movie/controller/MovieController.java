abc702a3b8b2c13f8b3a886210d7cf4f7d10839f
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2026-03-05 15:45:29 +0900

    chore: remove MySQL connector and unused dependencies

--- src/main/java/com/leemovie/movie/controller/MovieController.java
+++ src/main/java/com/leemovie/movie/controller/MovieController.java
@@ -1,4 +1,30 @@
 package com.leemovie.movie.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.leemovie.movie.domain.Movie;
+import com.leemovie.movie.service.MovieService;
+
+@Controller
 public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/movies")
+    public String movieList(Model model) {
+
+        List<Movie> movies = movieService.getMovies();
+
+        model.addAttribute("movies", movies);
+
+        return "movie-list";
+    }
 }
